0e31970ced033b8134b345926b46aa1754704ee7
Daniel Agapov <danielagapov1@gmail.com>
2024-11-04 19:41:27 -0800

    Created separate views

---
+++ Spawn-App-iOS-SwiftUI/Views/EventView.swift
@@ -0,0 +1,21 @@
+//
+//  EventView.swift
+//  Spawn-App-iOS-SwiftUI
+//
+//  Created by Daniel on 11/4/24.
+//
+
+import SwiftUI
+
+struct EventView: View {
+    var body: some View {
+        HStack{
+            Text("dinner time")
+        }
+            .padding()
+            .background()
+            .cornerRadius(20)
+            .shadow(radius: 5)
+    }
+        
+}
---
+++ Spawn-App-iOS-SwiftUI/Views/TagButtonView.swift
@@ -0,0 +1,34 @@
+//
+//  TagButtonView.swift
+//  Spawn-App-iOS-SwiftUI
+//
+//  Created by Daniel on 11/4/24.
+//
+
+import SwiftUI
+
+struct TagButtonView: View {
+    let mockTag: String
+    @Binding var activeTag: String
+    var animation: Namespace.ID
+    
+    var body: some View {
+        Button(action: {
+            withAnimation(.easeIn) {
+                activeTag = mockTag
+            }
+        }) {
+            Text(mockTag)
+                .font(.callout)
+                .foregroundColor(activeTag == mockTag ? .white : .black)
+                .padding(.vertical, 8)
+                .padding(.horizontal, 15)
+                .background {
+                    Capsule()
+                        .fill(activeTag == mockTag ? .black : .white)
+                        .matchedGeometryEffect(id: "ACTIVETAG_\(mockTag)", in: animation) // Use unique ID for each tag
+                }
+        }
+        .buttonStyle(.plain)
+    }
+}
