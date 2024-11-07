2024b2008c1ad7b216978a520ccc7c4efe1f8591
Daniel Agapov <danielagapov1@gmail.com>
2024-11-06 22:15:08 -0800

    Mocked `FriendTag`

--- Spawn-App-iOS-SwiftUI/Models/FriendTag.swift
+++ Spawn-App-iOS-SwiftUI/Models/FriendTag.swift
@@ -8,7 +8,16 @@
 // The idea is to have the friends the logged in user has marked
 // with a certain tag be contained within that friend tag.
 
+// Tech note: when filtering feed based on friend tags, I can simply check
+// the events against the event creator and see whether the event creator is
+// one of the `FriendTag`'s `friends`.
+
 struct FriendTag {
     var displayName: String
     var friends: [AppUser]?
+}
+
+extension FriendTag {
+    static let close = FriendTag(displayName: "Close Friends", friends: [AppUser.danielAgapov])
+    static let sports = FriendTag(displayName: "Sports", friends: [AppUser.danielLee])
 }
