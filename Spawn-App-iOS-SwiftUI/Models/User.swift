920a1ac182801601ce347530c1c36899682c414e
Daniel Agapov <danielagapov1@gmail.com>
2024-11-06 22:06:47 -0800

    Created `User` model

---
+++ Spawn-App-iOS-SwiftUI/Models/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  Spawn-App-iOS-SwiftUI
+//
+//  Created by Daniel on 11/6/24.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    var id: UUID
+    var friends: [User]?
+}
+
+
+extension User {
+    static let danielAgapov: User = User(id: UUID())
+    
+    static let danielLee: User = User(id: UUID(), friends: [danielAgapov])
+    
+    static let mockUsers: [User] = [
+        danielAgapov,
+        danielLee
+    ]
+}
