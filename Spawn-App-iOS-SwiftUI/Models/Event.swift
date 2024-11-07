2b686983e3bce369f497ff13bd321c0e4e1b3028
Daniel Agapov <danielagapov1@gmail.com>
2024-11-06 22:36:25 -0800

    Added `invited` (`[User]`) property to event, with tech note about how it'll be populated from friend tags

--- Spawn-App-iOS-SwiftUI/Models/Event.swift
+++ Spawn-App-iOS-SwiftUI/Models/Event.swift
@@ -22,6 +22,11 @@ struct Event: Identifiable, Codable {
     // tech note: I'll be able to check if current user is in an event's partipants to determine which symbol to show in feed
     var participants: [User]?
     var chatMessages: [ChatMessage]?
+    
+    // tech note: this will be determined by the `AppUser`'s specified `FriendTag`s on
+    // the event, which will populate this `invited` property with the `FriendTag`s'
+    // `friends` property (`[AppUser]`), which all have a `baseUser` (`User`) property.
+    var invited: [User]?
 }
 
 extension Event {
