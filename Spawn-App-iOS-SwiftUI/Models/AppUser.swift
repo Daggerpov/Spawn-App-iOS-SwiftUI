cae7b9af8709bf8d39656f39269c8e7074a5de99
Daniel Agapov <danielagapov1@gmail.com>
2024-11-06 22:07:59 -0800

    Created `AppUser` to build on `baseUser` property, of type `User`, to store app-specific info such as `bio` and `profilePicture`

---
+++ Spawn-App-iOS-SwiftUI/Models/AppUser.swift
@@ -0,0 +1,28 @@
+//
+//  AppUser.swift
+//  Spawn-App-iOS-SwiftUI
+//
+//  Created by Daniel on 11/6/24.
+//
+
+import SwiftUI
+
+// My idea for now is that I'll be able to link a `User` to its `AppUser`, such
+// that the `User` struct still comforms to `Codable` (which an `Image` property
+// would prevent if I included it in that struct).
+
+struct AppUser: Identifiable {
+    var id: UUID
+    var baseUser: User
+    var profilePicture: Image?
+    var username: String
+    var firstName: String?
+    var lastName: String?
+    var bio: String?
+}
+
+extension AppUser {
+    static let danielAgapov: AppUser = AppUser(id: UUID(), baseUser: User.danielAgapov, username: "daggerpov", firstName: "Daniel", lastName: "Agapov", bio: "This is my bio.")
+        
+    static let danielLee: AppUser = AppUser(id: UUID(), baseUser: User.danielLee, username: "uhdlee", firstName: "Daniel", lastName: "Lee", bio: "This is my bio.")
+}
