d93f36e41996051ad433b67f6fbc2ef27d88b013
Daniel Agapov <danielagapov1@gmail.com>
2024-11-06 22:22:32 -0800

    Created `Location` (stub model for now)

---
+++ Spawn-App-iOS-SwiftUI/Models/Location.swift
@@ -0,0 +1,11 @@
+//
+//  Location.swift
+//  Spawn-App-iOS-SwiftUI
+//
+//  Created by Daniel on 11/6/24.
+//
+
+struct Location {
+    // MARK: stub for now, but will make more robust later after implementing map
+    var coordinates: [Double]
+}
