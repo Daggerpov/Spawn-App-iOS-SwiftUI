d627346910567095695695ac47391a6161360ab3
Daniel Agapov <danielagapov1@gmail.com>
2024-11-06 22:31:47 -0800

    Created `Location` with just `locationName` for now, no precise coordinates

--- Spawn-App-iOS-SwiftUI/Models/Location.swift
+++ Spawn-App-iOS-SwiftUI/Models/Location.swift
@@ -5,7 +5,11 @@
 //  Created by Daniel on 11/6/24.
 //
 
-struct Location {
+struct Location: Codable {
     // MARK: stub for now, but will make more robust later after implementing map
-    var coordinates: [Double]
+    var locationName: String
+}
+
+extension Location {
+    static let mockLocation = Location(locationName: "Gold's Gym")
 }
