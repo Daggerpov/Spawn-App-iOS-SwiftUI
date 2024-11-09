e4779fa9240a89faca8f47e04f2a11fd1be41420
Daniel Agapov <danielagapov1@gmail.com>
2024-11-09 15:37:38 -0800

    app user look-up dictionary by user id

--- Spawn-App-iOS-SwiftUI/ViewModels/EventDescriptionViewModel.swift
+++ Spawn-App-iOS-SwiftUI/ViewModels/EventDescriptionViewModel.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 class EventDescriptionViewModel: ObservableObject {
+    @Published var appUsers: [AppUser]
+    var event: Event
     
+    init(event: Event, appUsers: [AppUser] = []) {
+        self.event = event
+        self.appUsers = appUsers
+    }
+    
+    public var appUserLookup: [UUID: AppUser] {
+        // look-up app user by id from user
+        Dictionary(uniqueKeysWithValues: appUsers.map { ($0.id, $0) })
+    }
 }
