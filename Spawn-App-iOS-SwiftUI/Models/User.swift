a11564cec21e2bca0955ddd9bc2fda96e5c1ee0a
Daniel Agapov <danielagapov1@gmail.com>
2024-11-09 15:06:54 -0800

    Fixed crashes from `User` model by initializing friends in separate function, rather than cyclically (crash cause)

--- Spawn-App-iOS-SwiftUI/Models/User.swift
+++ Spawn-App-iOS-SwiftUI/Models/User.swift
@@ -18,23 +18,31 @@ class User: Identifiable, Codable {
 }
 
 extension User {
-	static let danielAgapov: User = {
-		let user = User(id: UUID())
-		user.friends = User.mockUsers.filter { $0.id != user.id }
-		return user
-	}()
-    static let danielLee: User = User(id: UUID(), friends: [danielAgapov])
-	static let shannon: User = User(id: UUID(), friends: [danielAgapov, danielLee])
-	static let jennifer: User = User(id: UUID(), friends: [danielAgapov, danielLee, shannon])
-	static let michael: User = User(id: UUID(), friends: [danielAgapov, danielLee, shannon, jennifer])
-	static let haley: User = User(id: UUID(), friends: [danielAgapov, danielLee, shannon, jennifer, michael])
-
-    static let mockUsers: [User] = [
-        danielAgapov,
-        danielLee,
-		shannon,
-		jennifer,
-		michael,
-		haley
-    ]
+    static let danielAgapov: User = User(id: UUID(), friends: [])
+    static let danielLee: User = User(id: UUID(), friends: [])
+    static let shannon: User = User(id: UUID(), friends: [])
+    static let jennifer: User = User(id: UUID(), friends: [])
+    static let michael: User = User(id: UUID(), friends: [])
+    static let haley: User = User(id: UUID(), friends: [])
+    
+    static func setupFriends() {
+        danielAgapov.friends = [danielLee, shannon, jennifer, michael, haley]
+        danielLee.friends = [danielAgapov]
+        shannon.friends = [danielAgapov, danielLee]
+        jennifer.friends = [danielAgapov, danielLee, shannon]
+        michael.friends = [danielAgapov, danielLee, shannon, jennifer]
+        haley.friends = [danielAgapov, danielLee, shannon, jennifer, michael]
+    }
+    
+    static let mockUsers: [User] = {
+        setupFriends()
+        return [
+            danielAgapov,
+            danielLee,
+            shannon,
+            jennifer,
+            michael,
+            haley
+        ]
+    }()
 }
