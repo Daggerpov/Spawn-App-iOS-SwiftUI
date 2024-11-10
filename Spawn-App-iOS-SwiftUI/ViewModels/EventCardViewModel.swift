389464dba6a53f4dc1644ddbae47532d05651b25
Daniel Agapov <danielagapov1@gmail.com>
2024-11-09 18:12:24 -0800

    Turned `isParticipating` into published var so view updates dynamically

--- Spawn-App-iOS-SwiftUI/ViewModels/EventCardViewModel.swift
+++ Spawn-App-iOS-SwiftUI/ViewModels/EventCardViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class EventCardViewModel: ObservableObject {
     @Published var eventTimeDisplayString: String = ""
+    @Published var isParticipating: Bool = false
     
     var appUser: AppUser
     var event: Event
@@ -37,11 +38,9 @@ class EventCardViewModel: ObservableObject {
     }
     
     /// returns whether the logged in app user is part of the event's participants array
-    public func isParticipating() -> Bool {
-        
-        return ((event.participants?.contains(where: { user in
+    public func fetchIsParticipating() -> Void {
+        self.isParticipating = ((event.participants?.contains(where: { user in
             user.id == appUser.id
         })) != nil)
     }
-                
 }
