a31d6d26ec66477f2ed7c2f46080af49381d4b91
Daniel Agapov <danielagapov1@gmail.com>
2024-11-09 22:41:45 -0800

    Always placing creator at beginning of participants images

--- Spawn-App-iOS-SwiftUI/Views/SubComponents/ParticipantsImagesView.swift
+++ Spawn-App-iOS-SwiftUI/Views/SubComponents/ParticipantsImagesView.swift
@@ -6,27 +6,34 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ParticipantsImagesView: View {
     var event: Event
     
     var body: some View {
-        if let participants = event.participants {
-            HStack{
-                Spacer()
-                ForEach(participants, id: \.self.id) { participant in
-                    if let appUserParticipant: AppUser = AppUserService.shared.appUserLookup[participant.id] {
-                        NavigationLink(
-                            destination: ProfileView(appUser: appUserParticipant),
-                            label: {
-                                 if let profilePicture = appUserParticipant.profilePicture {
-                                     profilePicture
-                                         .ProfileImageModifier(
-                                            imageType: .eventParticipants
-                                         )
+        // sort by creator first, then rest:
+        let participants = [event.creator] + (event.participants ?? [])
+            .sorted { participant1, participant2 in
+                if participant1.id == event.creator.id { return true }
+                // not sure if this line is actually necessary:
+                if participant2.id == event.creator.id { return false }
+                return false
+            }
+        
+        HStack {
+            Spacer()
+            ForEach(participants, id: \.self.id) { participant in
+                if let appUserParticipant = AppUserService.shared.appUserLookup[participant.id] {
+                    NavigationLink(
+                        destination: ProfileView(appUser: appUserParticipant),
+                        label: {
+                            if let profilePicture = appUserParticipant.profilePicture {
+                                profilePicture
+                                    .ProfileImageModifier(imageType: .eventParticipants)
                             }
-                        })
-                    }
+                        }
+                    )
                 }
             }
         }
