cd8fbf2cc8db712e67e588e92537b9b35e8e2391
Daniel Agapov <danielagapov1@gmail.com>
2024-11-09 17:38:48 -0800

    working navigation

---
+++ Spawn-App-iOS-SwiftUI/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Spawn-App-iOS-SwiftUI
+//
+//  Created by Daniel Agapov on 11/9/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var appUser: AppUser
+    
+    var body: some View {
+        NavigationStack{
+            Text("as;ldfkj")
+        }
+    }
+}
