71587bf9f3069ecc99b1c926fd123fc38ecf19b5
Daniel Agapov <danielagapov1@gmail.com>
2024-11-11 11:47:47 -0800

    Created `BottomNavButtonView`

---
+++ Spawn-App-iOS-SwiftUI/Views/SubComponents/BottomNavButtonView.swift
@@ -0,0 +1,75 @@
+//
+//  BottomNavButtonView.swift
+//  Spawn-App-iOS-SwiftUI
+//
+//  Created by Daniel Agapov on 11/11/24.
+//
+
+import SwiftUI
+
+struct BottomNavButtonView: View {
+    var buttonType: BottomNavButtonType
+    var imageName: String
+    
+    init(buttonType: BottomNavButtonType) {
+        self.buttonType = buttonType
+        switch(buttonType) {
+            case .map:
+                self.imageName = "map.fill"
+            case .plus:
+                self.imageName = "plus"
+            case .tag:
+                self.imageName = "tag.fill"
+        }
+    }
+    
+    var body: some View {
+        if buttonType == .map || buttonType == .tag {
+            Circle()
+                .frame(width: 45, height: 45)
+                .foregroundColor(Color(hex: "#C0BCB4"))
+//                .background(Color(hex: "#C0BCB4"))
+                .clipShape(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(Color(hex: "#173131"), lineWidth: 2)
+                )
+                .overlay(
+                    NavigationLink(destination: {
+                        FriendMapView()
+                    }) {
+                        Image(systemName: imageName)
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .clipShape(Circle())
+                            .shadow(radius: 20)
+                            .foregroundColor(Color(hex: "#173131"))
+                    }
+                )
+        } else if buttonType == .plus {
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 45, height: 45)
+                .foregroundColor(Color(hex: "#C0BCB4"))
+//                .background(Color(hex: "#C0BCB4"))
+                .clipShape(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(Color(hex: "#173131"), lineWidth: 2)
+                )
+                .overlay(
+                    NavigationLink(destination: {
+                        FriendMapView()
+                    }) {
+                        Image(systemName: imageName)
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .clipShape(Circle())
+                            .shadow(radius: 20)
+                            .foregroundColor(Color(hex: "#173131"))
+                    }
+                )
+        }
+            
+        
+    }
+}
