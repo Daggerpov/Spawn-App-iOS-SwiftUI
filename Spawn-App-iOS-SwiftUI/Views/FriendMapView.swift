a65d06688e27332298fb98e349739371f136b997
Daniel Agapov <danielagapov1@gmail.com>
2024-11-11 11:47:54 -0800

    `FriendMapView` basics

---
+++ Spawn-App-iOS-SwiftUI/Views/FriendMapView.swift
@@ -0,0 +1,16 @@
+//
+//  FriendMapView.swift
+//  Spawn-App-iOS-SwiftUI
+//
+//  Created by Daniel Agapov on 11/11/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct FriendMapView: View {
+    
+    var body: some View {
+        Map()
+    }
+}
