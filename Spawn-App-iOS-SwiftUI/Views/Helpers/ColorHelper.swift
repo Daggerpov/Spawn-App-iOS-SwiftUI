0552441247cec68ea22713c93112b0bd76a74f72
Daniel Agapov <danielagapov1@gmail.com>
2024-11-14 14:29:10 -0800

    Variables for colours, replacing hard-coded, and renaming

--- Spawn-App-iOS-SwiftUI/Views/Helpers/ColorHelper.swift
+++ Spawn-App-iOS-SwiftUI/Views/Helpers/ColorHelper.swift
@@ -29,4 +29,5 @@ extension Color {
     }
 }
 
-let colors: [Color] = [Color(hex: "#8084ac"), Color(hex: "#704444"), Color(hex: "#b0442c"), Color(hex: "#889c6c")]
+let eventColors: [Color] = [Color(hex: "#8084ac"), Color(hex: "#704444"), Color(hex: "#b0442c"), Color(hex: "#889c6c")]
+let backgroundColor: Color = Color(hex: "#C0BCB4")
