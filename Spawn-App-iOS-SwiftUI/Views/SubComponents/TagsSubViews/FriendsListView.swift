7cc766ad015645557ffcb75bf4f700aeef23be63
Daniel Agapov <danielagapov1@gmail.com>
2024-11-14 16:27:12 -0800

    friend list view, which loops through friends, creating `FriendListingView` for each

--- Spawn-App-iOS-SwiftUI/Views/SubComponents/TagsSubViews/FriendsListView.swift
+++ Spawn-App-iOS-SwiftUI/Views/SubComponents/TagsSubViews/FriendsListView.swift
@@ -15,18 +15,45 @@ struct FriendsListView: View {
     }
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 20) {
-                // Title and Time Information
-                ForEach(viewModel.friends) { friend in
-                    Text(friend.username)
+        VStack {
+            // TODO: implement searching by friend username or either name
+            searchView
+            ScrollView {
+                VStack(alignment: .leading, spacing: 10) {
+                    // Title and Time Information
+                    ForEach(viewModel.friends) { friend in
+                        FriendListingView(friend: friend)
+                    }
                 }
             }
-            .padding(20)
-            .background(universalAccentColor)
-            .cornerRadius(universalRectangleCornerRadius)
         }
-        .padding(.horizontal) // Reduces padding on the bottom
+        .padding()
+        .background(universalBackgroundColor)
+        .padding(.horizontal)
         .padding(.top, 200)
+    }
+}
+
+extension FriendsListView {
+    var searchView: some View {
+        VStack(spacing: 10) {
+            HStack(spacing: 12) {
+                Image(systemName: "magnifyingglass").font(.title3)
+                TextField("Search", text: $viewModel.searchText)
+                // TODO: implement an onchange to filter the users by search text:
+//                    .onChange(of: viewModel.searchText) { _ in
+//                        viewModel.loadQuotesBySearch()
+//                    }
+            }
+            .foregroundColor(universalBackgroundColor)
+            .padding(.vertical, 20)
+            .padding(.horizontal, 15)
+            .frame(height: 45)
+            .background{
+                RoundedRectangle(cornerRadius: 25).fill(.background)
+                    .foregroundColor(universalBackgroundColor)
+            }
+        }
+        .padding(.vertical, 20)
     }
 }
