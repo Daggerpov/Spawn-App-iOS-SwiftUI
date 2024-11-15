2fdd3f075c8c86ff2e6a2b58c10791054cad4640
Daniel Agapov <danielagapov1@gmail.com>
2024-11-14 18:09:26 -0800

    fetching recommended friends, for non-friends in listing view

--- Spawn-App-iOS-SwiftUI/ViewModels/FriendsListViewModel.swift
+++ Spawn-App-iOS-SwiftUI/ViewModels/FriendsListViewModel.swift
@@ -9,12 +9,14 @@ import Foundation
 
 class FriendsListViewModel: ObservableObject {
     @Published var friends: [AppUser] = []
+    @Published var recommendedFriends: [AppUser] = []
     @Published var searchText: String = ""
     var appUser: AppUser
     
     init(appUser: AppUser) {
         self.appUser = appUser
         self.friends = fetchFriends()
+        self.recommendedFriends = fetchRecommendedFriends()
     }
     
     func fetchFriends() -> [AppUser] {
@@ -24,6 +26,19 @@ class FriendsListViewModel: ObservableObject {
             AppUserService.shared.appUserLookup[friend.id] ?? AppUser.emptyUser
         }
     }
+    
+    func fetchRecommendedFriends() -> [AppUser] {
+        let allMockUsers = AppUser.mockAppUsers
+        
+        let recommended = allMockUsers.filter { mockUser in
+            // Check if the mock user is not in the friends list
+            // and if the mock user isn't the own user themselves
+            return mockUser.id != appUser.id && !self.friends.contains(where: { $0.id == mockUser.id })
+        }
+        
+        return recommended
+    }
+
     
     func fetchFriendTags() -> [FriendTag] {
         return appUser.friendTags ?? []
