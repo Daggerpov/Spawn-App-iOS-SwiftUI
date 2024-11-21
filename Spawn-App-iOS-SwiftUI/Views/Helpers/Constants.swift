cdd8fc548833a1f50930da7884c1589dabec0c10
Daniel Agapov <danielagapov1@gmail.com>
2024-11-21 01:19:39 -0800

    simplify code in constants to get color from hex code

--- Spawn-App-iOS-SwiftUI/Views/Helpers/Constants.swift
+++ Spawn-App-iOS-SwiftUI/Views/Helpers/Constants.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 let universalRectangleCornerRadius: CGFloat = 20
 let eventColorHexCodes: [String] = ["#8084ac", "#8084ac", "#8084ac", "#8084ac"]
-let eventColors: [Color] = [Color(hex: eventColorHexCodes[0]), Color(hex: eventColorHexCodes[1]), Color(hex: eventColorHexCodes[2]), Color(hex: eventColorHexCodes[3])]
+let eventColors = eventColorHexCodes.map { colorHexCode in
+    Color(hex: colorHexCode)
+}
 let universalBackgroundColor: Color = Color(hex: "#E7E7DD")
 let universalAccentColor: Color = Color(hex: "#203434") // TODO: might want to define this in assets folder instead
