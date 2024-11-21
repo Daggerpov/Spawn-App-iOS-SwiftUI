1f210e6b7298152fffaa6f79171a814cdeece3b5
Daniel Agapov <danielagapov1@gmail.com>
2024-11-21 01:18:28 -0800

    using hex codes in globals

--- Spawn-App-iOS-SwiftUI/Views/Helpers/Constants.swift
+++ Spawn-App-iOS-SwiftUI/Views/Helpers/Constants.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 let universalRectangleCornerRadius: CGFloat = 20
-let eventColors: [Color] = [Color(hex: "#8084ac"), Color(hex: "#704444"), Color(hex: "#b0442c"), Color(hex: "#889c6c")]
+let eventColorHexCodes: [String] = ["#8084ac", "#8084ac", "#8084ac", "#8084ac"]
+let eventColors: [Color] = [Color(hex: eventColorHexCodes[0]), Color(hex: eventColorHexCodes[1]), Color(hex: eventColorHexCodes[2]), Color(hex: eventColorHexCodes[3])]
 let universalBackgroundColor: Color = Color(hex: "#E7E7DD")
 let universalAccentColor: Color = Color(hex: "#203434") // TODO: might want to define this in assets folder instead
