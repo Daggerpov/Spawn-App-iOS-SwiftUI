6156f9d35c4a89582a81c815030b55d49841644c
Daniel Agapov <danielagapov1@gmail.com>
2024-11-22 22:52:31 -0800

    Location model to match back-end

--- Spawn-App-iOS-SwiftUI/Models/Location.swift
+++ Spawn-App-iOS-SwiftUI/Models/Location.swift
@@ -5,16 +5,24 @@
 //  Created by Daniel Agapov on 11/6/24.
 //
 
-class Location: Codable {
-    // MARK: stub for now, but will make more robust later after implementing map
-    var locationName: String
-    
+import Foundation
 
-	init(locationName: String) {
-		self.locationName = locationName
+class Location: Identifiable, Codable {
+    var id: UUID
+    var locationName: String
+    var latitude: Double
+    var longitude: Double
+
+    init(id: UUID, locationName: String, latitude: Double, longitude: Double) {
+        self.id = id
+        self.locationName = locationName
+        self.latitude = latitude
+        self.longitude = longitude
 	}
 }
 
 extension Location {
-    static let mockLocation = Location(locationName: "Gold's Gym")
+    static let goldsGym = Location(id: UUID(), locationName: "Gold's Gym", latitude: 49.26629781435629, longitude: -123.24236920903301)
+    static let amsNest = Location(id: UUID(), locationName: "AMS Nest", latitude: 49.26672332535917, longitude: -123.2500705312989)
+    static let ikbLibrary = Location(id: UUID(), locationName: "IKB Library", latitude: 49.26764036247616, longitude: -123.25272383355049)
 }
