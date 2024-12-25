faa6cc5d474f15cc005c11a2f270f21e8107fe85
Daggerpov <danielagapov1@gmail.com>
2024-12-25 07:59:10 -0500

    Hot-Fix: `owner` -> `ownerId` for `FriendTag` mdoel

--- Spawn-App-iOS-SwiftUI/Models/User.swift
+++ Spawn-App-iOS-SwiftUI/Models/User.swift
@@ -15,223 +15,184 @@ import SwiftUI
 // we could also search by first name and last name, if provided (thus, optional types).
 
 struct User: Identifiable, Codable, Hashable {
-    static func == (lhs: User, rhs: User) -> Bool {
-        return lhs.id == rhs.id
-    }
+	static func == (lhs: User, rhs: User) -> Bool {
+		return lhs.id == rhs.id
+	}
 
-    var id: UUID
-    var friends: [User]?
-    var username: String
-    var profilePicture: String? // TODO: re-think data type later
-    var firstName: String?
-    var lastName: String?
-    var bio: String?
-    var friendTags: [FriendTag]?
-    var email: String
-    
-    init(
-        id: UUID,
-        friends: [User]? = nil,
-        username: String,
-        profilePicture: String? = nil,
-        firstName: String? = nil,
-        lastName: String? = nil,
-        bio: String? = nil,
-        friendTags: [FriendTag]? = nil,
-        email: String
-    ) {
-        self.id = id
-        self.friends = friends
-        self.username = username
-        self.profilePicture = profilePicture
-        self.firstName = firstName
-        self.lastName = lastName
-        self.bio = bio
-        self.friendTags = friendTags
-        self.email = email
-        
-        // Add friends to the user's default "Everyone" tag
-        if let friends = friends {
-            var everyoneTag = FriendTag(
-                id: UUID(),
-                displayName: "Everyone",
-				colorHexCode: "#asdfdf", owner: User.danielAgapov,
-                friends: []
-            )
-            
-            everyoneTag.friends = friends
-            
-            // Insert the "Everyone" tag at the beginning of the friend's tags array
-            self.friendTags?.insert(everyoneTag, at: 0)
-        }
-    }
+	var id: UUID
+	var friends: [User]?
+	var username: String
+	var profilePicture: String?  // TODO: re-think data type later
+	var firstName: String?
+	var lastName: String?
+	var bio: String?
+	var friendTags: [FriendTag]?
+	var email: String
+
+	init(
+		id: UUID,
+		friends: [User]? = nil,
+		username: String,
+		profilePicture: String? = nil,
+		firstName: String? = nil,
+		lastName: String? = nil,
+		bio: String? = nil,
+		friendTags: [FriendTag]? = nil,
+		email: String
+	) {
+		self.id = id
+		self.friends = friends
+		self.username = username
+		self.profilePicture = profilePicture
+		self.firstName = firstName
+		self.lastName = lastName
+		self.bio = bio
+		self.friendTags = friendTags
+		self.email = email
+
+		// Add friends to the user's default "Everyone" tag
+		if let friends = friends {
+			var everyoneTag = FriendTag(
+				id: UUID(),
+				displayName: "Everyone",
+				colorHexCode: "#asdfdf", ownerId: User.danielAgapov.id,
+				friends: []
+			)
+
+			everyoneTag.friends = friends
+
+			// Insert the "Everyone" tag at the beginning of the friend's tags array
+			self.friendTags?.insert(everyoneTag, at: 0)
+		}
+	}
 }
 
 extension User {
-    static var danielAgapov: User = User(
-        id: UUID(),
-        friends: [],
-        username: "daggerpov",
-        profilePicture: "Daniel_Agapov_pfp",
-        firstName: "Daniel",
-        lastName: "Agapov",
-        bio: "This is my bio.",
-		friendTags: [FriendTag(id: UUID(), displayName: "Hobbies", colorHexCode: eventColorHexCodes.randomElement() ?? "#ffffff", owner: User.danielAgapov, friends: [User.danielLee])],
-        email: "daniel@agapov.com"
-    )
-    
-    static var shannon: User = User(
-        id: UUID(),
-        friends: [],
-        username: "shannonaurl",
-        profilePicture: "Shannon_pfp",
-        firstName: "Shannon",
-        bio: "This is my bio.",
-        email: "shannon@gmail.com"
-    )
-    static var jennifer: User = User(
-        id: UUID(),
-        friends: [],
-        username: "jenntjen",
-        profilePicture: "Jennifer_pfp",
-        firstName: "Jennifer",
-        lastName: "Tjen",
-        bio: "This is my bio.",
-        email: "jennifer@gmail.com"
-    )
-    static var michael: User = User(
-        id: UUID(),
-        friends: [],
-        username: "michaeltham",
-        profilePicture: "Michael_pfp",
-        firstName: "Michael",
-        lastName: "Tham",
-        bio: "This is my bio.",
-        email: "haley@gmail.com"
-    )
-    static var haley: User = User(
-        id: UUID(),
-        friends: [],
-        username: "haleyusername",
-        profilePicture: "Haley_pfp",
-        firstName: "Haley",
-        bio: "This is my bio.",
-        email: "haley@gmail.com"
-    )
-    
-    static let emptyUser: User = User(
-        id: UUID(),
-        friends: [],
-        username: "empty username",
-        bio: "This is my bio.",
-        email: "haley@gmail.com"
-    )
-    
-    // MARK: this method of setting up danielLee's friends
-    // is messy, but works. The `setupFriends()` method
-    // below doesn't quite do the job.
-    
-    // Can't be asked to fix this, since it's just for mocking
-    
-    static var danielLeeFrienddanielAgapov: User = User(
-        id: UUID(),
-        friends: [],
-        username: "daggerpov",
-        profilePicture: "Daniel_Agapov_pfp",
-        firstName: "Daniel",
-        lastName: "Agapov",
-        bio: "This is my bio.",
-        email: "daniel@agapov.com"
-    )
-    
-    static var danielLeeFriendshannon: User = User(
-        id: UUID(),
-        friends: [],
-        username: "shannonaurl",
-        profilePicture: "Shannon_pfp",
-        firstName: "Shannon",
-        bio: "This is my bio.",
-        email: "shannon@gmail.com"
-    )
-    static var danielLeeFriendjennifer: User = User(
-        id: UUID(),
-        friends: [],
-        username: "jenntjen",
-        profilePicture: "Jennifer_pfp",
-        firstName: "Jennifer",
-        lastName: "Tjen",
-        bio: "This is my bio.",
-        email: "jennifer@gmail.com"
-    )
-    static var danielLeeFriendmichael: User = User(
-        id: UUID(),
-        friends: [],
-        username: "michaeltham",
-        profilePicture: "Michael_pfp",
-        firstName: "Michael",
-        lastName: "Tham",
-        bio: "This is my bio.",
-        email: "haley@gmail.com"
-    )
-    static var danielLeeFriendhaley: User = User(
-        id: UUID(),
-        friends: [],
-        username: "haleyusername",
-        profilePicture: "Haley_pfp",
-        firstName: "Haley",
-        bio: "This is my bio.",
-        email: "haley@gmail.com"
-    )
-    
-    static var danielLee: User = User(
-        id: UUID(),
-        friends: [
-            danielLeeFriendhaley,
-            danielLeeFriendmichael,
-            danielLeeFriendshannon,
-            danielLeeFriendjennifer,
-            danielLeeFrienddanielAgapov
-        ],
-        username: "uhdlee",
-        profilePicture: "Daniel_Lee_pfp",
-        firstName: "Daniel",
-        lastName: "Lee",
-        bio: "This is my bio.",
-        friendTags: [
-            FriendTag(
-                id: UUID(),
-                displayName: "Biztech",
-                colorHexCode: eventColorHexCodes[0],
-				owner: User.danielLee,
-                friends: [User.shannon]
-            ),
-            FriendTag(
-                id: UUID(),
-                displayName: "Close Friends",
+	static var danielAgapov: User = User(
+			id: UUID(),
+			username: "daggerpov",
+			profilePicture: "Daniel_Agapov_pfp",
+			firstName: "Daniel",
+			lastName: "Agapov",
+			bio: "This is my bio.",
+			email: "daniel@agapov.com"
+		)
+
+	static var danielLee: User = {
+		let friends: [User] = [shannon, jennifer, michael, haley]
+		let id: UUID = UUID()
+		return User(
+			id: id,
+			friends: friends,
+			username: "uhdlee",
+			profilePicture: "Daniel_Lee_pfp",
+			firstName: "Daniel",
+			lastName: "Lee",
+			bio: "This is my bio.",
+			friendTags: [
+				FriendTag(
+					id: UUID(),
+					displayName: "Biztech",
+					colorHexCode: eventColorHexCodes[0],
+					ownerId: id,
+					friends: [shannon]
+				),
+				FriendTag(
+					id: UUID(),
+					displayName: "Close Friends",
+					colorHexCode: eventColorHexCodes[1],
+					ownerId: id,
+					friends: [haley]
+				),
+				FriendTag(
+					id: UUID(),
+					displayName: "Hobbies",
+					colorHexCode: eventColorHexCodes[2],
+					ownerId: id,
+					friends: [jennifer, haley, shannon]
+				),
+			],
+			email: "daniel2456@gmail.com"
+		)
+	}()
+
+	static func setupFriends() {
+		// Set up mutual friends after all static properties are initialized
+		danielAgapov.friends = [shannon, jennifer, michael, haley]
+		danielAgapov.friendTags = [
+			FriendTag(
+				id: UUID(),
+				displayName: "Biztech",
+				colorHexCode: eventColorHexCodes[0],
+				ownerId: danielAgapov.id,
+				friends: [shannon]
+			),
+			FriendTag(
+				id: UUID(),
+				displayName: "Close Friends",
 				colorHexCode: eventColorHexCodes[1],
-				owner: User.danielLee,
-                friends: [User.haley]
-            ),
-            FriendTag(
-                id: UUID(),
-                displayName: "Hobbies",
+				ownerId: danielAgapov.id,
+				friends: [haley]
+			),
+			FriendTag(
+				id: UUID(),
+				displayName: "Hobbies",
 				colorHexCode: eventColorHexCodes[2],
-				owner: User.danielLee,
-                friends: [User.jennifer, User.haley, User.shannon]
-            )
-        ],
-        email: "daniel2456@gmail.com"
-    )
-    
-    static func setupFriends() {
-        danielAgapov.friends = [shannon, jennifer, michael, haley]
-        shannon.friends = [danielAgapov]
-        jennifer.friends = [danielAgapov, shannon]
-        michael.friends = [danielAgapov, shannon, jennifer]
-        haley.friends = [danielAgapov, shannon, jennifer, michael]
-    }
-    
-    static let mockUsers: [User] = {
-        return [danielAgapov, shannon, jennifer, michael, haley]
-    } ()
+				ownerId: danielAgapov.id,
+				friends: [jennifer, haley, shannon]
+			),
+		]
+
+		shannon.friends = [danielAgapov]
+		jennifer.friends = [danielAgapov, shannon]
+		michael.friends = [danielAgapov, shannon, jennifer]
+		haley.friends = [danielAgapov, shannon, jennifer, michael]
+
+	}
+
+	static var shannon: User = User(
+		id: UUID(),
+		friends: [],
+		username: "shannonaurl",
+		profilePicture: "Shannon_pfp",
+		firstName: "Shannon",
+		bio: "This is my bio.",
+		email: "shannon@gmail.com"
+	)
+
+	static var jennifer: User = User(
+		id: UUID(),
+		friends: [],
+		username: "jenntjen",
+		profilePicture: "Jennifer_pfp",
+		firstName: "Jennifer",
+		lastName: "Tjen",
+		bio: "This is my bio.",
+		email: "jennifer@gmail.com"
+	)
+
+	static var michael: User = User(
+		id: UUID(),
+		friends: [],
+		username: "michaeltham",
+		profilePicture: "Michael_pfp",
+		firstName: "Michael",
+		lastName: "Tham",
+		bio: "This is my bio.",
+		email: "haley@gmail.com"
+	)
+
+	static var haley: User = User(
+		id: UUID(),
+		friends: [],
+		username: "haleyusername",
+		profilePicture: "Haley_pfp",
+		firstName: "Haley",
+		bio: "This is my bio.",
+		email: "haley@gmail.com"
+	)
+
+	static let mockUsers: [User] = {
+		return [danielAgapov, shannon, jennifer, michael, haley]
+	}()
 }
