d21fdf6f01295110c2695e1d9974e28e722cf136
Daggerpov <danielagapov1@gmail.com>
2024-12-30 11:34:08 -0500

    Moved ObservableUser to `User` model file

--- Spawn-App-iOS-SwiftUI/Spawn_App_iOS_SwiftUIApp.swift
+++ Spawn-App-iOS-SwiftUI/Spawn_App_iOS_SwiftUIApp.swift
@@ -9,51 +9,10 @@ import SwiftUI
 
 @main
 struct Spawn_App_iOS_SwiftUIApp: App {
-    @StateObject var observableUser = ObservableUser(user: .danielAgapov) // Shared instance
-    
     var body: some Scene {
         WindowGroup {
-            FeedView()
-                .environmentObject(observableUser) // Inject the observable user into the environment
-				.onAppear {
-					User.setupFriends()
-				}
+			LaunchView()
 		}
-
     }
 }
 
-class ObservableUser: ObservableObject {
-    @Published private(set) var user: User
-    
-    init(user: User) {
-        self.user = user
-    }
-    
-    var id: UUID {
-        user.id
-    }
-    
-    var friends: [User]? {
-        user.friends
-    }
-    var username: String {
-        user.username
-    }
-    var profilePicture: String? {
-        user.profilePicture
-    }
-    
-    var firstName: String? {
-        user.firstName
-    }
-    var lastName: String? {
-        user.lastName
-    }
-    var bio: String? {
-        user.bio
-    }
-    var friendTags: [FriendTag]? {
-        user.friendTags
-    }
-}
