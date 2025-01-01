7a0a1c46baee16ae99c363cbdb78d20d2404f2b8
Daggerpov <danielagapov1@gmail.com>
2025-01-01 05:51:36 -0500

    Loop injection for profile going back to logout -> `LaunchView()`

--- Spawn-App-iOS-SwiftUI/Views/ProfileView.swift
+++ Spawn-App-iOS-SwiftUI/Views/ProfileView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ProfileView: View {
+	@EnvironmentObject var observableUser: ObservableUser
+
     let user: User
     @State private var bio: String
     @State private var editingState: ProfileEditText = .edit
@@ -82,7 +84,7 @@ struct ProfileView: View {
 
 					NavigationLink(destination: {
 						LaunchView()
-							.environmentObject(user)
+							.environmentObject(observableUser)
 							.onAppear {
 								User.setupFriends()
 							}
