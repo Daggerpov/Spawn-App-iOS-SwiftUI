d2f5dffb0ad54e7563cbeb4802349f7645a6be45
Daggerpov <danielagapov1@gmail.com>
2025-01-02 05:29:08 -0500

    `EventCreationButtonView` to reduce repeated code between `Feed` & `Map` Views

---
+++ Spawn-App-iOS-SwiftUI/Views/SubComponents/FeedSubViews/EventCreationButtonView.swift
@@ -0,0 +1,38 @@
+//
+//  EventCreationButton.swift
+//  Spawn-App-iOS-SwiftUI
+//
+//  Created by Daniel Agapov on 2025-01-02.
+//
+
+import SwiftUI
+
+struct EventCreationButtonView: View {
+	@Binding var showingEventCreationPopup: Bool
+    var body: some View {
+		RoundedRectangle(cornerRadius: universalRectangleCornerRadius)
+			.frame(width: 100, height: 45)
+			.foregroundColor(universalBackgroundColor)
+			.overlay(
+				RoundedRectangle(cornerRadius: universalRectangleCornerRadius)
+					.stroke(universalAccentColor, lineWidth: 2)
+			)
+			.overlay(
+				Button(action: {
+					showingEventCreationPopup = true
+				}) {
+					HStack{
+						Spacer()
+						Image(systemName: "plus")
+							.resizable()
+							.frame(width: 20, height: 20)
+							.clipShape(Circle())
+							.shadow(radius: 20)
+							.foregroundColor(universalAccentColor)
+							.font(.system(size: 30, weight: .bold))
+						Spacer()
+					}
+				}
+			)
+    }
+}
