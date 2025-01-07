204ac5b3443e5b544e0a837f9351fd309b9dbbb9
Daggerpov <danielagapov1@gmail.com>
2025-01-06 17:16:05 -0800

    dependency injecting mock vs. API service depending on static var, `mocking`

--- Spawn-App-iOS-SwiftUI/Services/API/MockAPIService.swift
+++ Spawn-App-iOS-SwiftUI/Services/API/MockAPIService.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 class MockAPIService: IAPIService {
+	/// This variable dictates whether we'll be using the `MockAPIService()` or `APIService()` throughout the app
+	static var mocking: Bool = false
+
 	func fetchData<T>(from url: URL) async throws -> T where T : Decodable {
 		if T.self == User.self {
 			return User.danielAgapov as! T
