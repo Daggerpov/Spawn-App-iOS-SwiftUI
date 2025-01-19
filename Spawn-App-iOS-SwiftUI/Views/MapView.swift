93303628a7c77d96e6140a39568f717e2262aa3c
Daggerpov <danielagapov1@gmail.com>
2025-01-18 21:27:19 -0800

    Essentially cherry-picking #81

--- Spawn-App-iOS-SwiftUI/Views/MapView.swift
+++ Spawn-App-iOS-SwiftUI/Views/MapView.swift
@@ -6,7 +6,6 @@
 //
 
 import MapKit
-import PopupView
 import SwiftUI
 
 struct MapView: View {
@@ -28,112 +27,146 @@ struct MapView: View {
 
 	@State private var showingEventCreationPopup: Bool = false
 
+	// for pop-ups:
+	@State private var descriptionOffset: CGFloat = 1000
+	@State private var creationOffset: CGFloat = 1000
+	// ------------
+
 	init(user: User) {
 		_viewModel = StateObject(wrappedValue: FeedViewModel(apiService: MockAPIService.isMocking ? MockAPIService() : APIService(), user: user))
 	}
 
 	var body: some View {
-		ZStack {
-			Map(
-				coordinateRegion: $region,
-				annotationItems: viewModel.events
-			) { mockEvent in
-				MapAnnotation(
-					coordinate: CLLocationCoordinate2D(
-						latitude: mockEvent.location?.latitude ?? 0,
-						longitude: mockEvent.location?.longitude ?? 0
-					), anchorPoint: CGPoint(x: 0.5, y: 1.0)
-				) {
-					Button(action: {
-						eventInPopup = mockEvent
-						colorInPopup = eventColors.randomElement()
-						showingEventDescriptionPopup = true
-					}) {
-						VStack(spacing: -8) {
-							ZStack {
-								Image(systemName: "mappin.circle.fill")
-									.resizable()
-									.scaledToFit()
-									.frame(width: 60, height: 60)
-									.foregroundColor(universalAccentColor)
-
-								let creatorOne: User = mockEvent.creator ?? User.danielAgapov
-
-								if let creatorPfp = creatorOne
-									.profilePicture
-								{
-									Image(creatorPfp)
-										.ProfileImageModifier(
-											imageType: .mapView)
+		ZStack{
+			ZStack {
+				Map(
+					coordinateRegion: $region,
+					annotationItems: viewModel.events
+				) { mockEvent in
+					MapAnnotation(
+						coordinate: CLLocationCoordinate2D(
+							latitude: mockEvent.location?.latitude ?? 0,
+							longitude: mockEvent.location?.longitude ?? 0
+						), anchorPoint: CGPoint(x: 0.5, y: 1.0)
+					) {
+						Button(action: {
+							eventInPopup = mockEvent
+							colorInPopup = eventColors.randomElement()
+							showingEventDescriptionPopup = true
+						}) {
+							VStack(spacing: -8) {
+								ZStack {
+									Image(systemName: "mappin.circle.fill")
+										.resizable()
+										.scaledToFit()
+										.frame(width: 60, height: 60)
+										.foregroundColor(universalAccentColor)
+									
+									let creatorOne: User = mockEvent.creator ?? User.danielAgapov
+									
+									if let creatorPfp = creatorOne
+										.profilePicture
+									{
+										Image(creatorPfp)
+											.ProfileImageModifier(
+												imageType: .mapView)
+									}
 								}
+								Triangle()
+									.fill(universalAccentColor)
+									.frame(width: 40, height: 20)
 							}
-							Triangle()
-								.fill(universalAccentColor)
-								.frame(width: 40, height: 20)
 						}
 					}
 				}
-			}
-			.ignoresSafeArea()
-			VStack {
+				.ignoresSafeArea()
 				VStack {
-					TagsScrollView(tags: viewModel.tags)
-				}
-				.padding(.horizontal)
-				.padding(.top, 20)
-				Spacer()
-				HStack(spacing: 35) {
-					BottomNavButtonView(buttonType: .feed, source: .map)
+					VStack {
+						TagsScrollView(tags: viewModel.tags)
+					}
+					.padding(.horizontal)
+					.padding(.top, 20)
 					Spacer()
-					EventCreationButtonView(
-						showingEventCreationPopup: $showingEventCreationPopup
-					)
-					Spacer()
-					BottomNavButtonView(buttonType: .friends, source: .map)
+					HStack(spacing: 35) {
+						BottomNavButtonView(buttonType: .feed, source: .map)
+						Spacer()
+						EventCreationButtonView(
+							showingEventCreationPopup: $showingEventCreationPopup
+						)
+						Spacer()
+						BottomNavButtonView(buttonType: .friends, source: .map)
+					}
+					.padding(32)
 				}
-				.padding(32)
+				.padding(.top, 50)
 			}
-			.padding(.top, 50)
-		}
-
-		.ignoresSafeArea()
-		.onAppear {
-			adjustRegionForEvents()
-			Task{
-				await viewModel.fetchEventsForUser()
-				await viewModel.fetchTagsForUser()
+			
+			.ignoresSafeArea()
+			.onAppear {
+				adjustRegionForEvents()
+				Task{
+					await viewModel.fetchEventsForUser()
+					await viewModel.fetchTagsForUser()
+				}
 			}
-		}
-		.popup(isPresented: $showingEventDescriptionPopup) {
-			if let event = eventInPopup, let color = colorInPopup {
-				EventDescriptionView(
-					event: event,
-					users: User.mockUsers,
-					color: color
-				)
+			if showingEventDescriptionPopup {
+				if let event = eventInPopup, let color = colorInPopup {
+					ZStack {
+						Color(.black)
+							.opacity(0.5)
+							.onTapGesture {
+								closeDescription()
+							}
+						
+						EventDescriptionView(
+							event: event,
+							users: User.mockUsers,
+							color: color
+						)
+						.fixedSize(horizontal: false, vertical: true)
+						.padding()
+						.background(.white)
+						.clipShape(RoundedRectangle(cornerRadius: 20))
+						.shadow(radius: 20)
+						.padding(30)
+						.offset(x: 0, y: descriptionOffset)
+						.onAppear {
+							withAnimation(.spring()) {
+								descriptionOffset = 0
+							}
+						}
+					}
+					.ignoresSafeArea()
+				}
 			}
-		} customize: {
-			$0
-				.type(
-					.floater(
-						verticalPadding: 20,
-						horizontalPadding: 20,
-						useSafeAreaInset: false
-					))
-			// TODO: read up on the documentation: https://github.com/exyte/popupview
-			// so that the description view is dismissed upon clicking outside
-		}
-		.popup(isPresented: $showingEventCreationPopup) {
-			EventCreationView(creatingUser: user.user)
-		} customize: {
-			$0
-				.type(.floater(
-					verticalPadding: 20,
-					horizontalPadding: 20,
-					useSafeAreaInset: false
-				))
-			// TODO: read up on the documentation: https://github.com/exyte/popupview
-			// so that the description view is dismissed upon clicking outside
+			if showingEventCreationPopup {
+				ZStack {
+					Color(.black)
+						.opacity(0.5)
+						.onTapGesture {
+							closeCreation()
+						}
+					
+					VStack{
+						Spacer()
+						EventCreationView(creatingUser: user.user)
+							.fixedSize(horizontal: false, vertical: true)
+							.padding()
+							.background(.white)
+							.clipShape(RoundedRectangle(cornerRadius: 20))
+							.shadow(radius: 20)
+							.padding(30)
+							.offset(x: 0, y: creationOffset)
+							.onAppear {
+								withAnimation(.spring()) {
+									creationOffset = 0
+								}
+							}
+						Spacer()
+					}
+				}
+				.ignoresSafeArea()
+			}
 		}
 	}
 	private func adjustRegionForEvents() {
@@ -156,6 +189,20 @@ struct MapView: View {
 			center: CLLocationCoordinate2D(latitude: centerLatitude, longitude: centerLongitude),
 			span: MKCoordinateSpan(latitudeDelta: max(latitudeDelta, 0.01), longitudeDelta: max(longitudeDelta, 0.01))
 		)
+	}
+
+	func closeDescription() {
+		withAnimation(.spring()) {
+			descriptionOffset = 1000
+			showingEventDescriptionPopup = false
+		}
+	}
+
+	func closeCreation() {
+		withAnimation(.spring()) {
+			creationOffset = 1000
+			showingEventCreationPopup = false
+		}
 	}
 }
 
