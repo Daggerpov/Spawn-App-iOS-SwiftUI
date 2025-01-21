9965209f0207138e13757564764825d0578bd5b4
Daggerpov <danielagapov1@gmail.com>
2025-01-20 17:30:48 -0800

    `isMocking` to true + absolute string comparisons for api calls from `FeedViewModel` and `FriendsTabViewModel`

--- Spawn-App-iOS-SwiftUI/Services/API/MockAPIService.swift
+++ Spawn-App-iOS-SwiftUI/Services/API/MockAPIService.swift
@@ -9,11 +9,51 @@ import Foundation
 
 class MockAPIService: IAPIService {
 	/// This variable dictates whether we'll be using the `MockAPIService()` or `APIService()` throughout the app
-	static var isMocking: Bool = false
+	static var isMocking: Bool = true
 
 	var errorMessage: String? = nil
 
+	var userId: UUID
+
+	init(userId: UUID) {
+		self.userId = userId
+	}
+
 	func fetchData<T>(from url: URL) async throws -> T where T : Decodable {
+		/// FeedViewModel.swift:
+
+		// fetchEventsForUser():
+
+		if url.absoluteString == APIService.baseURL + "events" {
+			return Event.mockEvents as! T
+		}
+
+		// fetchTagsForUser():
+
+		if url.absoluteString == APIService.baseURL + "friendTags" {
+			return FriendTag.mockTags as! T
+		}
+
+		/// FriendsTabViewModel.swift:
+
+		// fetchIncomingFriendRequests():
+
+		if url.absoluteString == APIService.baseURL + "users/\(userId)/friend-requests" {
+			return FriendRequest.mockFriendRequests as! T
+		}
+
+		// fetchRecommendedFriends():
+
+		if url.absoluteString == APIService.baseURL + "users/\(userId)/recommended-friends" {
+			return User.mockUsers as! T
+		}
+
+		// fetchFriends():
+
+		if url.absoluteString == APIService.baseURL + "users/\(userId)/friends" {
+			return User.mockUsers as! T
+		}
+
 		if T.self == User.self {
 			return User.danielAgapov as! T
 		}
