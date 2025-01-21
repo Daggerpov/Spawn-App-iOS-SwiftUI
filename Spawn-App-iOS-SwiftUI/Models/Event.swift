2c4ac7947b588f3e461f1e9e7e7e386a88ab9199
Daggerpov <danielagapov1@gmail.com>
2025-01-20 19:42:05 -0800

    Just replaced iso date formatter calls with current `Date()`

--- Spawn-App-iOS-SwiftUI/Models/Event.swift
+++ Spawn-App-iOS-SwiftUI/Models/Event.swift
@@ -64,8 +64,8 @@ extension Event {
 	static let mockDinnerEvent: Event = Event(
 		id: UUID(),
 		title: "Dinner time!!!!!!",
-		startTime: iso8601DateFormatter.date(from: "2025-01-10T22:00:00Z"),
-		endTime: iso8601DateFormatter.date(from: "2025-01-10T23:30:00Z"),
+		startTime: Date(),
+		endTime: Date(),
 		location: Location(id: UUID(), name: "Gather - Place Vanier", latitude: 49.26468617023799, longitude: -123.25859833051356),
 		note: "let's eat!",
 		creator: User.jennifer,
@@ -82,7 +82,7 @@ extension Event {
 		Event(
 			id: UUID(),
 			title: "wanna run 5k with me?",
-			startTime: iso8601DateFormatter.date(from: "2025-01-11T16:00:00Z"),
+			startTime: Date(),
 			location: Location(id: UUID(), name: "Wesbrook Mall", latitude: 49.25997722657244, longitude: -123.23986523529379),
 			creator: User.danielAgapov,
 			participants: [User.danielAgapov, User.jennifer, User.shannon, User.haley, User.danielLee],
@@ -106,7 +106,7 @@ extension Event {
 		Event(
 			id: UUID(),
 			title: "playing basketball!!!",
-			endTime: iso8601DateFormatter.date(from: "2025-01-11T19:00:00Z"),
+			endTime: Date(),
 			location: Location(id: UUID(), name: "UBC Student Recreation Centre", latitude: 49.2687302352351, longitude: -123.24897582888525),
 			note: "let's play basketball!",
 			creator: User.danielAgapov
@@ -114,8 +114,8 @@ extension Event {
 		Event(
 			id: UUID(),
 			title: "Im painting rn lol",
-			startTime: iso8601DateFormatter.date(from: "2025-01-11T10:00:00Z"),
-			endTime: iso8601DateFormatter.date(from: "2025-01-11T11:30:00Z"),
+			startTime: Date(),
+			endTime: Date(),
 			location: Location(id: UUID(), name: "Ross Drive - Wesbrook Mall", latitude: 49.25189587512135, longitude: -123.237051932404),
 			creator: User.shannon,
 			participants: [User.danielLee]
@@ -123,24 +123,24 @@ extension Event {
 		Event(
 			id: UUID(),
 			title: "Grabbing Udon",
-			startTime: iso8601DateFormatter.date(from: "2025-01-11T12:00:00Z"),
-			endTime: iso8601DateFormatter.date(from: "2025-01-11T14:30:00Z"),
+			startTime: Date(),
+			endTime: Date(),
 			location: Location(id: UUID(), name: "Marugame Udon", latitude: 49.28032597998406, longitude: -123.11026665974741),
 			creator: User.danielAgapov
 		),
 		Event(
 			id: UUID(),
 			title: "Calendar Party",
-			startTime: iso8601DateFormatter.date(from: "2025-01-11T23:00:00Z"),
-			endTime: iso8601DateFormatter.date(from: "2025-01-12T02:30:00Z"),
+			startTime: Date(),
+			endTime: Date(),
 			location: Location(id: UUID(), name: "The Pit - Nest", latitude: 49.26694140754859, longitude: -123.25036565366581),
 			creator: User.danielLee
 		),
 		Event(
 			id: UUID(),
 			title: "Gym - Leg Day",
-			startTime: iso8601DateFormatter.date(from: "2025-01-11T10:00:00Z"),
-			endTime: iso8601DateFormatter.date(from: "2025-01-11T11:30:00Z"),
+			startTime: Date(),
+			endTime: Date(),
 			location: Location(id: UUID(), name: "UBC Student Recreation Centre", latitude: 49.2687302352351, longitude: -123.24897582888525),
 			creator: User.michael
 		)
