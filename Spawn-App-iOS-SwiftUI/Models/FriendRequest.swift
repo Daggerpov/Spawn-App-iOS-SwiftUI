3783868c49c9fdf373c374ca2aad9414f09e4146
Daggerpov <danielagapov1@gmail.com>
2025-01-20 16:25:09 -0800

    created FriendRequest model

---
+++ Spawn-App-iOS-SwiftUI/Models/FriendRequest.swift
@@ -0,0 +1,25 @@
+//
+//  FriendRequest.swift
+//  Spawn-App-iOS-SwiftUI
+//
+//  Created by Daniel Agapov on 2025-01-20.
+//
+
+import Foundation
+
+/// as defined in the back-end `FriendRequestDTO.java`
+struct FriendRequest: Identifiable, Codable, Hashable {
+	static func == (lhs: FriendTag, rhs: FriendTag) -> Bool {
+		return lhs.id == rhs.id
+	}
+
+	var id: UUID
+	var senderUserId: UUID
+	var receiverUserId: UUID
+
+	init (id: UUID, senderUserId: UUID, receiverUserId: UUID) {
+		self.id = id
+		self.senderUserId = senderUserId
+		self.receiverUserId = receiverUserId
+	}
+}
