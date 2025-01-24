535a29f206e01a8fac5f3dfd5d8b4cb25857eb52
Daggerpov <danielagapov1@gmail.com>
2025-01-24 13:39:20 -0800

    Google auth button switch

--- Spawn-App-iOS-SwiftUI/Views/Authentication/LaunchView.swift
+++ Spawn-App-iOS-SwiftUI/Views/Authentication/LaunchView.swift
@@ -6,6 +6,18 @@
 //
 
 import SwiftUI
+import GoogleSignInSwift
+import GoogleSignIn
+
+import UIKit
+
+func getRootViewController() -> UIViewController? {
+	guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+		  let rootViewController = windowScene.windows.first?.rootViewController else {
+		return nil
+	}
+	return rootViewController
+}
 
 struct LaunchView: View {
 	@StateObject var viewModel: LaunchViewModel = LaunchViewModel(
@@ -27,11 +39,32 @@ struct LaunchView: View {
 						.navigationBarTitle("")
 						.navigationBarHidden(true)
 				}) {
-					AuthProviderButtonView(authProviderType: .google)
-				}.simultaneousGesture(
-					TapGesture().onEnded {
-						loginWithGoogle()
-					})
+					GoogleSignInButton {
+						if let rootViewController = getRootViewController() {
+							GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { signInResult, error in
+//								if let error = error as? NSError {
+//									if error.code == GIDSignInErrorCode.canceled.rawValue {
+//										print("Sign-In was canceled by the user.")
+//									} else {
+//										print("Google Sign-In Error: \(error.localizedDescription)")
+//									}
+//									return
+//								}
+
+								// Handle successful sign-in
+								if let user = signInResult?.user {
+									print("Signed in as: \(user.profile?.name ?? "Unknown")")
+								}
+							}
+						} else {
+							print("Unable to find root view controller for sign-in.")
+						}
+					}
+				}
+//				.simultaneousGesture(
+//					TapGesture().onEnded {
+//						loginWithGoogle()
+//					})
 
 				NavigationLink(destination: {
 					UserInfoInputView()
@@ -54,17 +87,6 @@ struct LaunchView: View {
 			}
 		}
 		.environmentObject(observableUser)
-	}
-
-	private func loginWithGoogle() {
-		guard
-			let url = URL(
-				string: "https://spawn-app-back-end-production.up.railway.app/oauth2/authorization/google")
-		else {
-			print("Invalid URL")
-			return
-		}
-		UIApplication.shared.open(url)
 	}
 
 	private func loginWithApple() {
