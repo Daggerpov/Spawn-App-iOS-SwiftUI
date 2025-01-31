7778212f43c6fd801452e3538b925e6ffc983a7d
Daggerpov <danielagapov1@gmail.com>
2025-01-30 17:27:44 -0800

    fix UI

--- Spawn-App-iOS-SwiftUI/Views/SubComponents/NewTagsSubViews/Tags/CreatingTagRowView.swift
+++ Spawn-App-iOS-SwiftUI/Views/SubComponents/NewTagsSubViews/Tags/CreatingTagRowView.swift
@@ -22,6 +22,7 @@ struct CreatingTagRowView: View {
 				Group {
 					TextField("Enter Name", text: $displayName)
 						.underline()
+						.background(.white)
 
 					Button(action: {
 						Task{
