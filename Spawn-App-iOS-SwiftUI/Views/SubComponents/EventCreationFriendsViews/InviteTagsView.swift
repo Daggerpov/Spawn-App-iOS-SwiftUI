b96f50c3b8b4f567332fc3d45fed1af62cdfa2c4
Daggerpov <danielagapov1@gmail.com>
2025-02-02 14:32:49 -0800

    new invite views

---
+++ Spawn-App-iOS-SwiftUI/Views/SubComponents/EventCreationFriendsViews/InviteTagsView.swift
@@ -0,0 +1,66 @@
+//
+//  InviteTagsView.swift
+//  Spawn-App-iOS-SwiftUI
+//
+//  Created by Daniel Agapov on 2025-02-02.
+//
+
+import SwiftUI
+
+struct InviteTagsView: View {
+	@ObservedObject var viewModel: TagsViewModel
+	@State private var creationStatus: CreationStatus = .notCreating
+
+	init(user: User) {
+		self.viewModel = TagsViewModel(
+			apiService: MockAPIService.isMocking
+			? MockAPIService(userId: user.id) : APIService(), user: user)
+	}
+
+	var body: some View {
+		VStack {
+			VStack(alignment: .leading, spacing: 15) {
+				Text("TAGS")
+					.font(.headline)
+					.foregroundColor(universalAccentColor)
+
+				AddTagButtonView(creationStatus: $creationStatus, color: universalAccentColor)
+					.environmentObject(viewModel)
+			}
+			Spacer()
+			Spacer()
+			tagsSection
+		}
+		.onAppear {
+			Task {
+				await viewModel.fetchTags()
+			}
+		}
+		.padding()
+	}
+}
+
+extension InviteTagsView {
+	var tagsSection: some View {
+		Group {
+			ScrollView {
+				VStack(spacing: 15) {
+					ForEach(viewModel.tags) { friendTag in
+						TagRow(friendTag: friendTag)
+							.background(
+								RoundedRectangle(cornerRadius: 12)
+									.fill(
+										Color(hex: friendTag.colorHexCode)
+											.opacity(0.5)
+									)
+									.cornerRadius(
+										universalRectangleCornerRadius
+									)
+							)
+							.environmentObject(viewModel)
+					}
+				}
+			}
+		}
+	}
+}
