3883812fe175a6bd9542d7a9204dd831455e5034
Daggerpov <danielagapov1@gmail.com>
2025-02-03 06:14:25 -0800

    Styling for inviting friend

--- Spawn-App-iOS-SwiftUI/Views/SubComponents/EventCreationFriendsViews/InviteFriendsView.swift
+++ Spawn-App-iOS-SwiftUI/Views/SubComponents/EventCreationFriendsViews/InviteFriendsView.swift
@@ -16,14 +16,14 @@ struct InviteFriendsView: View {
 		self.viewModel = FriendsTabViewModel(
 			userId: user.id,
 			apiService: MockAPIService.isMocking
-			? MockAPIService(userId: user.id) : APIService())
+				? MockAPIService(userId: user.id) : APIService())
 	}
 
 	var body: some View {
 		ScrollView {
 			VStack {
 				// TODO: maybe we can implement this later for searching through friends
-//				SearchView(searchPlaceholderText: "search or add friends")
+				//				SearchView(searchPlaceholderText: "search or add friends")
 			}
 			friendsSection
 		}
@@ -58,57 +58,71 @@ struct InviteFriendsView: View {
 
 struct IndividualFriendView: View {
 	var friend: FriendUserDTO
+	@State private var isSelected: Bool = false
 
 	var body: some View {
-		HStack {
-			if let pfp = friend.profilePicture {
-				Image(pfp)
-					.resizable()
-					.scaledToFill()
-					.frame(width: 60, height: 60)
-					.clipShape(Circle())
+		Button(action: {
+			if isSelected {
+				isSelected = false
+			} else {
+				isSelected = true
 			}
+		}) {
+			HStack {
+				if let pfp = friend.profilePicture {
+					Image(pfp)
+						.resizable()
+						.scaledToFill()
+						.frame(width: 60, height: 60)
+						.clipShape(Circle())
+				}
 
-			VStack(alignment: .leading, spacing: 8) {
-				Text(friend.username)
-					.font(.system(size: 16, weight: .bold))
-					.foregroundColor(universalBackgroundColor)
+				VStack(alignment: .leading, spacing: 8) {
+					HStack {
+						Image(systemName: "star.fill")
+							.font(.callout)
+						Text(friend.username)
+							.font(.system(size: 16, weight: .bold))
+					}
+					.foregroundColor(isSelected ? .white : universalAccentColor)
 
-				HStack(spacing: 8) {
-					ForEach(
-						friend.associatedFriendTagsToOwner ?? []
-					) { friendTag in
-						Text(friendTag.displayName)
-							.font(
-								.system(size: 14, weight: .medium)
-							)
-							.padding(.horizontal, 12)
-							.padding(.vertical, 6)
-							.background(Color(hex: friendTag.colorHexCode))
-							.foregroundColor(.white)
-							.cornerRadius(12)
+					HStack(spacing: 8) {
+						ForEach(
+							friend.associatedFriendTagsToOwner ?? []
+						) { friendTag in
+							Text(friendTag.displayName)
+								.font(
+									.system(size: 10, weight: .medium)
+								)
+								.padding(.horizontal, 12)
+								.padding(.vertical, 6)
+								.background(Color(hex: friendTag.colorHexCode))
+								.foregroundColor(.white)
+								.cornerRadius(12)
+						}
 					}
 				}
-			}
-			.padding(.leading, 8)
+				.padding(.leading, 8)
 
-			Spacer()
-		}
-		.padding(.vertical, 16)
-		.padding(.horizontal, 20)
-		.background(universalBackgroundColor)
-		.cornerRadius(24)
-		.overlay {
-			RoundedRectangle(
-				cornerRadius: universalRectangleCornerRadius
+				Spacer()
+			}
+			.padding(.vertical, 16)
+			.padding(.horizontal, 20)
+			.background(
+				isSelected ? universalAccentColor : universalBackgroundColor
 			)
-			.stroke(universalAccentColor, lineWidth: 2)
+			.cornerRadius(24)
+			.overlay {
+				RoundedRectangle(
+					cornerRadius: universalRectangleCornerRadius
+				)
+				.stroke(universalAccentColor, lineWidth: 2)
+			}
 		}
 	}
 }
 
 @available(iOS 17.0, *)
-#Preview
-{
+#Preview {
 	InviteFriendsView(user: .danielLee)
 }
