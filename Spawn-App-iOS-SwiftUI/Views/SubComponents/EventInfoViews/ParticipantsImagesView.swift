633c62a83071893234f365286d5c7f01bc8fe51c
Daggerpov <danielagapov1@gmail.com>
2025-02-12 16:40:39 -0800

    re-visited logic (didn't modify) of participantsCleanup

--- Spawn-App-iOS-SwiftUI/Views/SubComponents/EventInfoViews/ParticipantsImagesView.swift
+++ Spawn-App-iOS-SwiftUI/Views/SubComponents/EventInfoViews/ParticipantsImagesView.swift
@@ -11,21 +11,17 @@ import Foundation
 struct ParticipantsImagesView: View {
     var event: Event
 
-	// TODO DANIEL: maybe revisit this logic later
     func participantsCleanup(participants: [User]) -> [User]{
         var participantsFiltered = participants
-        // Remove the creator if already in the list
-		let userCreator: User = event.creatorUser ?? User.danielAgapov
-        participantsFiltered.removeAll { $0.id == userCreator.id }
 
-        // Prepend the creator to the participants list
-		participantsFiltered.insert(event.creatorUser ?? User.danielAgapov, at: 0)
+		if let userCreator: User = event.creatorUser {
+			// Remove the creator if already in the list
+			participantsFiltered.removeAll { $0.id == userCreator.id }
 
-        // Sort the rest of the participants (if necessary)
-        participantsFiltered.sort { participant1, participant2 in
-            // Creator is already at the front, so this sorting can handle the rest
-            return false // maintain original order
-        }
+			// Prepend the creator to the participants list
+			participantsFiltered.insert(event.creatorUser ?? User.danielAgapov, at: 0)
+		}
+
         return participantsFiltered
     }
     
