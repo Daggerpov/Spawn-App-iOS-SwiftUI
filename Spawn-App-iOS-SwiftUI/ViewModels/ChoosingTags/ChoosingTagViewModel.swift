5d521b7b8ba8b4237ccba4e1a492c4b96d560c03
miketham24 <michaelthamk@gmail.com>
2025-02-14 15:59:54 -0800

    adding addtagstofriend func

--- Spawn-App-iOS-SwiftUI/ViewModels/ChoosingTags/ChoosingTagViewModel.swift
+++ Spawn-App-iOS-SwiftUI/ViewModels/ChoosingTags/ChoosingTagViewModel.swift
@@ -8,7 +8,35 @@
 import Foundation
 
 class ChooseTagPopUpViewModel: ObservableObject {
+    @Published var chooseTagErrorMessage: String = ""
     
     //TODO: figure out functions
     
+    var apiService: IAPIService
+    
+    init(apiService: IAPIService) {
+        self.apiService = apiService
+    }
+    
+    func AddTagsToFriend(friendUserId: UUID, friendTagIds: [UUID]) async
+    {
+        if let url = URL(
+            string: APIService.baseURL + "friendTags/addUserToTags")
+        {
+            do {
+                try await self.apiService.sendData(EmptyBody(),
+to: url,
+                    parameters: [
+                        "userId": friendUserId.uuidString,
+                        "friendTagIds": friendTagIds,
+                    ])
+            } catch {
+                await MainActor.run {
+                    chooseTagErrorMessage =
+                        "There was an error select a tag for your friend. Please try again."
+                    print(apiService.errorMessage ?? "")
+                }
+            }
+        }
+    }
 }
