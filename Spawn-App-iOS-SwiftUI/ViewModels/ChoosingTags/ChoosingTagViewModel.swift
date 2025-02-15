f97dcaebf018d14802576e9db9d2d955d6e8237a
Daggerpov <danielagapov1@gmail.com>
2025-02-14 16:03:03 -0800

    updated method

--- Spawn-App-iOS-SwiftUI/ViewModels/ChoosingTags/ChoosingTagViewModel.swift
+++ Spawn-App-iOS-SwiftUI/ViewModels/ChoosingTags/ChoosingTagViewModel.swift
@@ -8,35 +8,37 @@
 import Foundation
 
 class ChooseTagPopUpViewModel: ObservableObject {
-    @Published var chooseTagErrorMessage: String = ""
-    
-    //TODO: figure out functions
-    
-    var apiService: IAPIService
-    
-    init(apiService: IAPIService) {
-        self.apiService = apiService
-    }
-    
-    func AddTagsToFriend(friendUserId: UUID, friendTagIds: [UUID]) async
-    {
-        if let url = URL(
-            string: APIService.baseURL + "friendTags/addUserToTags")
-        {
-            do {
-                try await self.apiService.sendData(EmptyBody(),
-to: url,
-                    parameters: [
-                        "userId": friendUserId.uuidString,
-                        "friendTagIds": friendTagIds,
-                    ])
-            } catch {
-                await MainActor.run {
-                    chooseTagErrorMessage =
-                        "There was an error select a tag for your friend. Please try again."
-                    print(apiService.errorMessage ?? "")
-                }
-            }
-        }
-    }
+	@Published var chooseTagErrorMessage: String = ""
+
+	//TODO: figure out functions
+
+	var apiService: IAPIService
+
+	init(apiService: IAPIService) {
+		self.apiService = apiService
+	}
+
+	func addTagsToFriend(friendUserId: UUID, friendTagIds: [UUID]) async {
+		if let url = URL(
+			string: APIService.baseURL + "friendTags/addUserToTags")
+		{
+			do {
+				// Convert the UUID array to an array of strings
+				let friendTagIdsString = friendTagIds.map { $0.uuidString }
+
+				try await self.apiService.sendData(
+					EmptyBody(), to: url,
+					parameters: [
+						"userId": friendUserId.uuidString as AnyObject,
+						"friendTagIds": friendTagIdsString as AnyObject,
+					])
+			} catch {
+				await MainActor.run {
+					chooseTagErrorMessage =
+						"There was an error selecting a tag for your friend. Please try again."
+					print(apiService.errorMessage ?? "")
+				}
+			}
+		}
+	}
 }
