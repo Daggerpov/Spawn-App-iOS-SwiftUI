023e8f5ec2a49daab62ddd2b19fd1080c96bf0d5
Daniel Agapov <danielagapov1@gmail.com>
2025-03-30 07:24:30 -0700

    useless methods

--- Spawn-App-iOS-SwiftUI/Services/FeedbackService.swift
+++ Spawn-App-iOS-SwiftUI/Services/FeedbackService.swift
@@ -40,12 +40,11 @@ class FeedbackService: ObservableObject {
                 image: image
             )
             
-            // Send to appropriate endpoint
-            if image != nil {
-                try await submitWithImage(feedback: feedbackDTO)
-            } else {
-                try await submitWithoutImage(feedback: feedbackDTO)
+            guard let url = URL(string: APIService.baseURL + "feedback") else {
+                throw APIError.URLError
             }
+            
+            let _ = try await apiService.sendData(feedbackDTO, to: url, parameters: nil)
             
             await MainActor.run {
                 isSubmitting = false
@@ -54,23 +53,6 @@ class FeedbackService: ObservableObject {
         } catch {
             await setError("Failed to submit feedback: \(error.localizedDescription)")
         }
-    }
-    
-    private func submitWithoutImage(feedback: CreateFeedbackSubmissionDTO) async throws {
-        guard let url = URL(string: APIService.baseURL + "feedback") else {
-            throw APIError.URLError
-        }
-        
-        let _ = try await apiService.sendData(feedback, to: url, parameters: nil)
-    }
-    
-    private func submitWithImage(feedback: CreateFeedbackSubmissionDTO) async throws {
-        guard let url = URL(string: APIService.baseURL + "feedback") else {
-            throw APIError.URLError
-        }
-        
-        // Use sendData instead of multipart form data
-        let _ = try await apiService.sendData(feedback, to: url, parameters: nil)
     }
     
     @MainActor
