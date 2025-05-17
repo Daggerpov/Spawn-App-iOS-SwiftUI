9dd83919088c460595c48ab8369974447d8648b4
Daniel Agapov <danielagapov1@gmail.com>
2025-05-17 14:51:40 -0700

    search bar view normalized

--- Spawn-App-iOS-SwiftUI/Views/Components/SearchBarButtonView.swift
+++ Spawn-App-iOS-SwiftUI/Views/Components/SearchBarButtonView.swift
@@ -9,38 +9,49 @@ import SwiftUI
 
 struct SearchBarButtonView: View {
     var placeholder: String
-    var action: () -> Void
+    var action: (() -> Void)? = nil
     
     var body: some View {
-        Button(action: action) {
-            HStack {
-                Image(systemName: "magnifyingglass")
-                    .font(.onestRegular(size: 18))
-                    .foregroundColor(.gray)
-                
-                Text(placeholder)
-                    .font(.onestRegular(size: 16))
-                    .foregroundColor(.gray)
-                
-                Spacer()
+        Group {
+            if let action = action {
+                Button(action: action) {
+                    searchBarContent
+                }
+                .buttonStyle(PlainButtonStyle())
+            } else {
+                searchBarContent
             }
-            .padding(.vertical, 12)
-            .padding(.horizontal, 16)
-            .background(
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(maxWidth: .infinity, minHeight: 46, maxHeight: 46)
-                    .cornerRadius(15)
-                    .overlay(
-                        RoundedRectangle(
-                            cornerRadius: universalRectangleCornerRadius
-                        )
-                            .inset(by: 0.75)
-                            .stroke(.gray)
-                    )
-            )
         }
-        .buttonStyle(PlainButtonStyle())
+    }
+    
+    private var searchBarContent: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .font(.onestRegular(size: 18))
+                .foregroundColor(.gray)
+            
+            Text(placeholder)
+                .font(.onestRegular(size: 16))
+                .foregroundColor(.gray)
+            
+            Spacer()
+        }
+        .padding(.vertical, 12)
+        .padding(.horizontal, 16)
+        .background(
+            Rectangle()
+                .foregroundColor(universalBackgroundColor)
+                .frame(maxWidth: .infinity, minHeight: 46, maxHeight: 46)
+                .cornerRadius(15)
+                .overlay(
+                    RoundedRectangle(
+                        cornerRadius: universalRectangleCornerRadius
+                    )
+                        .inset(by: 0.75)
+                        .stroke(.gray)
+                )
+        )
+        .colorScheme(.light)
     }
 }
 
