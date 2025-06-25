c9e94753e2b2895b90eb615c9b61a616a93c94d1
Daggerpov <danielagapov1@gmail.com>
2025-06-24 19:59:17 -0700

    Revert "fix font registration bugs"
    This reverts commit 2136b55f1d052f23a6090d2417db8fe36970fe27.

--- Spawn-App-iOS-SwiftUI/Extensions/Font+Extensions.swift
+++ Spawn-App-iOS-SwiftUI/Extensions/Font+Extensions.swift
@@ -1,6 +1,28 @@
 import SwiftUI
 
 extension Font {
+    static func registerFonts() {
+        // Only call this once, typically in the app startup
+        registerFont(bundle: .main, fontName: "Onest-Regular", fontExtension: "ttf")
+        registerFont(bundle: .main, fontName: "Onest-Medium", fontExtension: "ttf")
+        registerFont(bundle: .main, fontName: "Onest-Bold", fontExtension: "ttf")
+        registerFont(bundle: .main, fontName: "Onest-SemiBold", fontExtension: "ttf")
+    }
+    
+    private static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
+        guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
+              let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
+              let font = CGFont(fontDataProvider) else {
+            print("Could not load font \(fontName).\(fontExtension)")
+            return
+        }
+        
+        var error: Unmanaged<CFError>?
+        if !CTFontManagerRegisterGraphicsFont(font, &error) {
+            print("Error registering font: \(error.debugDescription)")
+        }
+    }
+    
     // Convenience methods for using Onest font
     static func onestRegular(size: CGFloat) -> Font {
         return .custom("Onest-Regular", size: size)
