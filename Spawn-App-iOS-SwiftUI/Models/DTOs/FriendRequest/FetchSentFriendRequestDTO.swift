c4743c21dd508436d070d898500a82f0b97d91a1
Daggerpov <danielagapov1@gmail.com>
2025-10-27 16:10:57 -0700

    simplify DTOs that were dealing with corrupted data

--- Spawn-App-iOS-SwiftUI/Models/DTOs/FriendRequest/FetchSentFriendRequestDTO.swift
+++ Spawn-App-iOS-SwiftUI/Models/DTOs/FriendRequest/FetchSentFriendRequestDTO.swift
@@ -20,43 +20,6 @@ struct FetchSentFriendRequestDTO: Identifiable, Codable, Hashable {
         self.id = id
         self.receiverUser = receiverUser
     }
-
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case receiverUser
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        // Decode receiver first
-        let decodedReceiverUser = try container.decode(BaseUserDTO.self, forKey: .receiverUser)
-
-        // Try to decode UUID directly; if null or missing, throw an error instead of using receiver ID
-        if let uuid = try? container.decode(UUID.self, forKey: .id) {
-            self.id = uuid
-        } else if let idString = try? container.decodeIfPresent(String.self, forKey: .id),
-                  let uuid = UUID(uuidString: idString) {
-            self.id = uuid
-        } else {
-            // If friend request ID is missing or invalid, this indicates a backend serialization issue
-            // Don't use receiver ID as fallback since it causes API calls to fail with wrong ID
-            throw DecodingError.dataCorrupted(
-                DecodingError.Context(
-                    codingPath: [CodingKeys.id],
-                    debugDescription: "Friend request ID is missing or invalid. Cannot use receiver ID as fallback."
-                )
-            )
-        }
-
-        self.receiverUser = decodedReceiverUser
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(receiverUser, forKey: .receiverUser)
-    }
 }
 
 extension FetchSentFriendRequestDTO {
